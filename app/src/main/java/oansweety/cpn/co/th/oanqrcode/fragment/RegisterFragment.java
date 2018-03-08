90a43b6e64b4cc0eafb2c3e01e764e194c7551cd
OanSweety <oansweety@gmail.com>
2018-03-08 10:35:23 +0700

    popToBack stack Success

--- app/src/main/java/oansweety/cpn/co/th/oanqrcode/fragment/RegisterFragment.java
+++ app/src/main/java/oansweety/cpn/co/th/oanqrcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package oansweety.cpn.co.th.oanqrcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import oansweety.cpn.co.th.oanqrcode.MainActivity;
 import oansweety.cpn.co.th.oanqrcode.R;
 
 /**
@@ -14,6 +16,40 @@ import oansweety.cpn.co.th.oanqrcode.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+//        Config Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title and SubTitle
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_register));
+
+//        Setup Navigator
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);        // Display, <- Button
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);   // Click, <- Button
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
