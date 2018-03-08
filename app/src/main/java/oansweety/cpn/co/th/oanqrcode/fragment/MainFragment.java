c82b98204cfb3ae1d46d90b0dbb9cd6f49761912
OanSweety <oansweety@gmail.com>
2018-03-08 09:43:17 +0700

    Replace Fragment

--- app/src/main/java/oansweety/cpn/co/th/oanqrcode/fragment/MainFragment.java
+++ app/src/main/java/oansweety/cpn/co/th/oanqrcode/fragment/MainFragment.java
@@ -1,11 +1,13 @@
 package oansweety.cpn.co.th.oanqrcode.fragment;
 
+import android.icu.text.Replaceable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import oansweety.cpn.co.th.oanqrcode.R;
 
@@ -14,6 +16,33 @@ import oansweety.cpn.co.th.oanqrcode.R;
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+
+    }   // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);  // Initial View
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
 
     @Nullable
     @Override
